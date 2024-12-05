d204aebb6cfd3018b4286dd877fa7469ab577fe7
unknown <B00346@microjet.com.tw>
2024-12-05 11:54:44 +0800

    fix env variable

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+from utility import air_conditioner_recommendation
+
+st.title("研能科技空調推薦系統 🆒")
+
+st.divider()
+# user_request=st.text_input(r"$\textsf{\Large 🌡️ 請輸入使用者之空間用途及使用坪數 🌡️}$")
+user_request=st.text_input("🌡️ 請輸入使用者之空間用途及使用坪數 🌡️")
+submit = st.button("❄️ 產生推薦空調型號 ❄️")
+
+
+
+if submit and not user_request:
+    st.info("请输入使用者之空間用途及使用坪數")
+    st.stop()
+
+if submit:
+    with st.spinner("AI正在運行中，請稍後..."):
+        result = air_conditioner_recommendation(user_request)
+        
+        
+    st.success("空調推薦系統執行完畢！")
+    st.subheader("推薦型號 ✇🌫 ：")
+    st.write(result)
+ 
+
---
+++ utility.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+import streamlit as st
+# Config Parse
+# from configparser import ConfigParser
+# config = ConfigParser()
+# config.read("config.ini")
+
+# key = config["OPEN_AI"]["API_KEY"]
+# key = st.secrets["openai"]["OPENAI_API_KEY"]
+# api_key = st.secrets["GOOGLE_API_KEY"] 
+key=st.secrets["OPENAI_API_KEY"]
+def air_conditioner_recommendation(user_request):
+    client = OpenAI(api_key=key)
+
+
+    completion = client.chat.completions.create(
+    model="ft:gpt-3.5-turbo-0125:personal:microjet-v2:AYROs5Ka",
+    messages=[
+        {"role": "system", "content": "You are a professional air conditioning technician who provides adviceon choosing suitable air conditioning models. below are the air conditioner models you can recommend: zhac1-2.9-o / zhac1-2.9-i, zhac1-3.6-o / zhac1-3.6-i, zhac1-4.1-o / zhac1-4.1-i, zhac1-5.2-o / zhac1-5.2-i, zhac1-6.3-o / zhac1-6.3-i, zhac1-7.3-o / zhac1-7.3-i."},  
+    
+        {"role": "user", "content": "user_request"}
+
+    ]
+    )
+  
+ 
+    return(completion.choices[0].message.content)
+    # return 
